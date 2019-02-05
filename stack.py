82312f0f31d0f7fbedb5f3ef4f9dbba00f4f7777
Saurabh <47058975+mr-vuln3rabl3@users.noreply.github.com>
2019-02-05 18:37:27 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,33 @@
+class Stack():
+	
+	
+	def __init__(self, limit=10):
+		self.stack=[]
+		self.limit=limit
+		
+	def __bool__(self):
+		return not bool(self.stack)
+	
+	def push(self, val):
+		if len(self.stack) >= self.limit:
+			return "Full"
+		stack.append(val)
+	
+	def pop(self, val):
+		if len(self.stack) == self.limit:
+			return self.stack.pop()
+		else:
+			return "nothing"
+			
+	def peep(self, val):
+		if len(self.stack) == self.limit:
+			return self.stack[-1]
+		
+if __name__ == '__main__':
+	stack=Stack()
+	for i in range(10):
+		stack.push(i)
+		
+print("Stack: " + str(stack))
+
+		
